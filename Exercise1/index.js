63cd757279b71389e89e447a44acb5e6f01cfea7
David Kananda <davidkananda@gmail.com>
2018-07-08 10:42:00 +0100

    exercise one complete

--- Exercise1/index.js
+++ Exercise1/index.js
@@ -2,15 +2,35 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7b7c75',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  redSquare: {
+    backgroundColor: '#f45342',
+    flexDirection: 'row',
+    width: 200,
+    height: 200
+  },
+  blueRectangle: {
+    backgroundColor: '#409bef',
+    flex: 2
+  },
+  greenRectangle: {
+    backgroundColor: '#2aaf59',
+    flex: 1
+  }
 
 });
 
 const Exercise1 = () => {
   return (
-    <View>
-      <View>
-        <View />
-        <View />
+    <View style={ styles.container }>
+      <View style={ styles.redSquare }>
+        <View style={ styles.blueRectangle } />
+        <View style={ styles.greenRectangle } />
       </View>
     </View>
   );
