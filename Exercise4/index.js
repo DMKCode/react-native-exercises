568d3f8b4aa38d3cf476edd556d6b7bd0b691ad2
David Kananda <davidkananda@gmail.com>
2018-07-23 06:28:44 +0100

    completed exercise 4

--- Exercise4/index.js
+++ Exercise4/index.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#c5e1f9',
+    flex: 1,
+  },
+  list: {
+    marginTop: 30
+  },
+  listItem: {
+    borderBottomWidth: 1,
+    padding: 5,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  listItemMainText: {
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  listItemSubText: {
+    fontSize: 14,
+  }
+});
 
 class Challenge3 extends Component {
   state = {
     people: [],
   }
+
+  _renderItem = ({item}) => (
+    <View style={styles.listItem}>
+      <Text style={styles.listItemMainText}>{item.name}</Text>
+      <Text style={styles.listItemSubText}>Birth Year: {item.birth_year}</Text>
+    </View>
+  ) 
+
+  _keyExtractor = (item, index) => index;
 
   componentDidMount() {
     fetch('https://swapi.co/api/people')
@@ -13,7 +53,17 @@ class Challenge3 extends Component {
   }
 
   render() {
-    return null;
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <FlatList
+          data={this.state.people}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+          style={styles.list}
+        />
+      </SafeAreaView>
+    );
   }
 }
 
